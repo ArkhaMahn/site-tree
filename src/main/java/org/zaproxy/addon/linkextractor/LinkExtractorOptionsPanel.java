7aae3a25e0ccf78fe7dbf06794c58ce609fb1b96
Arkhamahn <arkhamahn@gmail.com>
2026-08-19 07:38:54 -0400

    Refactor LinkExtractorOptionsPanel: add thread slider panel with label as header above slider, slider spans full width, remove thread counter label

--- src/main/java/org/zaproxy/addon/linkextractor/LinkExtractorOptionsPanel.java
+++ src/main/java/org/zaproxy/addon/linkextractor/LinkExtractorOptionsPanel.java
@@ -2,17 +2,21 @@ package org.zaproxy.addon.linkextractor;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
 import org.parosproxy.paros.Constant;
 import org.parosproxy.paros.model.OptionsParam;
 import org.parosproxy.paros.view.AbstractParamPanel;
 import org.zaproxy.zap.utils.ZapHtmlLabel;
 
 /**
- * Options panel shown under <em>Tools &gt; Options &gt; Sites tree</em>.
+ * Options panel shown under <em>Tools > Options > Sites tree</em>.
  *
- * <p>Provides toggles for the two optional behaviours of the {@link LinkExtractorNetworkListener}:
- * JavaScript parsing and subdomain discovery.
+ * <p>Provides toggles for the optional behaviours of the {@link LinkExtractorNetworkListener}:
+ * JavaScript parsing, subdomain discovery, and thread concurrency.
  */
 public class LinkExtractorOptionsPanel extends AbstractParamPanel {
 
@@ -24,6 +28,7 @@ public class LinkExtractorOptionsPanel extends AbstractParamPanel {
 
     private JCheckBox parseJavascriptCheckBox;
     private JCheckBox discoverSubdomainsCheckBox;
+    private JSlider threadsSlider;
 
     public LinkExtractorOptionsPanel(LinkExtractorOptionsParam optionsParam) {
         super();
@@ -53,9 +58,14 @@ public class LinkExtractorOptionsPanel extends AbstractParamPanel {
         add(getDiscoverSubdomainsCheckBox(), gbc);
 
         gbc.gridy = 3;
+        gbc.insets = new Insets(8, 0, 0, 0);
+        add(getThreadsPanel(), gbc);
+
+        gbc.gridy = 4;
         gbc.weighty = 1.0;
         gbc.fill = GridBagConstraints.BOTH;
-        add(new javax.swing.JPanel(), gbc);
+        gbc.insets = new Insets(0, 0, 0, 0);
+        add(new JPanel(), gbc);
     }
 
     private JCheckBox getParseJavascriptCheckBox() {
@@ -73,12 +83,47 @@ public class LinkExtractorOptionsPanel extends AbstractParamPanel {
         return discoverSubdomainsCheckBox;
     }
 
+    private JPanel getThreadsPanel() {
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.insets = new Insets(0, 20, 0, 0);
+        panel.add(new JLabel(Constant.messages.getString(PREFIX + ".label.threads")), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(4, 20, 0, 0);
+        panel.add(getThreadsSlider(), gbc);
+        return panel;
+    }
+
+    private JSlider getThreadsSlider() {
+        if (threadsSlider == null) {
+            threadsSlider = new JSlider(
+                    LinkExtractorOptionsParam.MIN_THREADS,
+                    LinkExtractorOptionsParam.MAX_THREADS,
+                    optionsParam.getThreads());
+            threadsSlider.setMajorTickSpacing(1);
+            threadsSlider.setMinorTickSpacing(1);
+            threadsSlider.setPaintTicks(true);
+            threadsSlider.setPaintLabels(true);
+            threadsSlider.setSnapToTicks(true);
+            threadsSlider.setToolTipText(Constant.messages.getString(PREFIX + ".label.threads.tooltip"));
+        }
+        return threadsSlider;
+    }
+
     @Override
     public void initParam(Object obj) {
         OptionsParam optionsParam = (OptionsParam) obj;
         LinkExtractorOptionsParam param = optionsParam.getParamSet(LinkExtractorOptionsParam.class);
         getParseJavascriptCheckBox().setSelected(param.isParseJavascript());
         getDiscoverSubdomainsCheckBox().setSelected(param.isDiscoverSubdomains());
+        threadsSlider.setValue(param.getThreads());
     }
 
     @Override
@@ -87,5 +132,6 @@ public class LinkExtractorOptionsPanel extends AbstractParamPanel {
         LinkExtractorOptionsParam param = optionsParam.getParamSet(LinkExtractorOptionsParam.class);
         param.setParseJavascript(getParseJavascriptCheckBox().isSelected());
         param.setDiscoverSubdomains(getDiscoverSubdomainsCheckBox().isSelected());
+        param.setThreads(threadsSlider.getValue());
     }
 }
